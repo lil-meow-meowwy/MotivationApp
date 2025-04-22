637a2270099b490825b948a2e2acf2beaf34faa2
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-04-22 22:57:26 +0300

    Favourites are saved even after closing the app (I hope)

--- MotivationQuotes/ContentView.swift
+++ MotivationQuotes/ContentView.swift
@@ -17,6 +17,22 @@ struct ContentView: View {
     @State private var currentQuote: Quote?
     @State private var favorites: [Quote] = []
 
+    let favoritesKey="savedQuotes"
+    
+    func saveFavorites() {
+        if let encoded=try? JSONEncoder().encode(favorites) {
+            UserDefaults.standard.set(encoded, forKey: favoritesKey)
+        }
+    }
+    
+    func loadFavorites() {
+        if let savedData=UserDefaults.standard.data(forKey: favoritesKey) {
+            let decoded=try? JSONDecoder().decode([Quote].self, from: savedData){
+                favorites=decoded
+            }
+        }
+    }
+
     var body: some View {
         ZStack {
             Color(.systemBackground).ignoresSafeArea()
@@ -57,7 +73,12 @@ struct ContentView: View {
                                 favorites.append(quote)
                             }
                         }) {
-                            Text("❤️ Save to Favorites")
+                            Text("❤️ Save to Favorites"){
+                                if !favorites.contains(where: {$0.text==quote.text}){
+                                    favorites.append(quote)
+                                    saveFavorites()
+                                }
+                            }
                                 .padding()
                                 .frame(maxWidth: .infinity)
                                 .background(Color.red)
@@ -75,11 +96,12 @@ struct ContentView: View {
                 .navigationTitle("Motivation App")
                 .onAppear {
                     fetchQuote()
+                    loadFavorites()
                 }
             }
         }
     }
-
+    
     func fetchQuote() {
         guard let url = URL(string: "https://zenquotes.io/api/random") else { return }
 
