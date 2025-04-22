5867409994b77a3f8be9d010b24fe6ff189ef798
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-04-22 23:19:21 +0300

    Sharing is caring

---
+++ MotivationQuotes/ActivityView.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityView.swift
+//  MotivationQuotes
+//
+//  Created by Kristina Yaroshenko on 22.04.2025.
+//
+
+import SwiftUI
+import UIKit
+
+struct ActivityView: UIViewControllerRepresentable {
+    let activityItems: [Any]
+    let applicationActivities: [UIActivity]? = nil
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        return UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities
+        )
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
